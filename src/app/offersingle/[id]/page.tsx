df828fa0dc143f914ca8e39009b9c2a8490d13da
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2024-12-04 18:01:55 +0600

    id

--- src/app/offersingle/[id]/page.tsx
+++ src/app/offersingle/[id]/page.tsx
@@ -1,18 +1,18 @@
+// "use client";
 import Container from "@/components/Container";
 import SingleService from "@/components/SingleService";
 import { mainService } from "@/constant";
 import { ProductItem } from "../../../../type";
 
-// interface PageProps {
-//   params: {
-//     id: number;
-//   };
-// }
-// @ts-ignore
-const OfferSinglePage = ({ params }) => {
+type Params = {
+  id: string; // Assuming the 'id' in URL is a string
+};
+
+const OfferSinglePage = ({ params }: { params: Params }) => {
   const { id } = params;
 
   const filterData = mainService.find((item) => item.id === Number(id));
+  console.log("fil", filterData);
   if (!filterData) {
     return (
       <Container className="">
