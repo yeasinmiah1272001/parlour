d55ed8f483acf81b4599f4e12718f2773c6581c3
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2024-12-05 18:22:43 +0600

    err

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import React from "react";
+
 import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
